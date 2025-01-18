70db96a2321af387f505e0ecff32eebfdd3405c2
Karan8113 <157044092+Karan8113@users.noreply.github.com>
2025-01-18 10:40:50 +0530

    Time: 15 ms (78.06%) | Memory: 26 MB (43.32%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,65 +1,44 @@
 class Solution {
 public:
-    int m,n;
-    bool isValid(int r,int c){
-        if(r<0 || r>=m || c<0 || c>=n){
-            return false;
-        }
-        return true;
+    int m, n;
+
+    bool isValid(int r, int c) {
+        return r >= 0 && r < m && c >= 0 && c < n;
     }
+
     int minCost(vector<vector<int>>& grid) {
-        m=grid.size();
-        n=grid[0].size();
+        m = grid.size();
+        n = grid[0].size();
 
-        vector<pair<int,int>> mp(5,{0,0});
-        mp[1]={0,1};
-        mp[2]={0,-1};
-        mp[3]={1,0};
-        mp[4]={-1,0};
+        vector<pair<int, int>> directions = {{0, 0}, {0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        deque<pair<int, int>> dq; // Deque for 0-1 BFS
+        vector<vector<int>> cost(m, vector<int>(n, INT_MAX));
+        cost[0][0] = 0;
 
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
-        pq.push({0,{0,0}});
-        
+        dq.push_front({0, 0});
 
-        vector<vector<int>> cost(m,vector<int>(n,INT_MAX));
-        vector<vector<int>> visited(m,vector<int>(n,0));
-        cost[0][0]=0;
-        int curRow,curCol,curCost,first,sec;
-        int newCost;
-        while(!pq.empty()){
-           
-            curCost=pq.top().first;
-            curRow=pq.top().second.first;
-            curCol=pq.top().second.second;
+        while (!dq.empty()) {
+            auto [curRow, curCol] = dq.front();
+            dq.pop_front();
 
-            pq.pop();
-            
+            for (int i = 1; i <= 4; i++) {
+                int newRow = curRow + directions[i].first;
+                int newCol = curCol + directions[i].second;
 
-            if(visited[curRow][curCol]==0){
-                for(int i=1;i<=4;i++){
-                    first=mp[i].first;
-                    sec=mp[i].second;
-
-                    if(isValid(curRow+first,curCol+sec)){
-                        if(i==grid[curRow][curCol]){
-                            newCost=curCost;
+                if (isValid(newRow, newCol)) {
+                    int newCost = cost[curRow][curCol] + (i != grid[curRow][curCol]);
+                    if (newCost < cost[newRow][newCol]) {
+                        cost[newRow][newCol] = newCost;
+                        if (i == grid[curRow][curCol]) {
+                            dq.push_front({newRow, newCol}); // No additional cost, add to the front
+                        } else {
+                            dq.push_back({newRow, newCol}); // Extra cost, add to the back
                         }
-                        else{
-                            newCost = curCost + 1;
-                        }
-                        if(cost[curRow+first][curCol+sec]>newCost){
-                            cost[curRow+first][curCol+sec]=newCost;
-                            pq.push({newCost,{curRow+first,curCol+sec}});
-                        }                    
                     }
-   
                 }
-
             }
-            visited[curRow][curCol]=1;
         }
 
-       
-        return cost[m-1][n-1];
+        return cost[m - 1][n - 1];
     }
 };
