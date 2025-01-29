4af92e3e138363050522fc625dbb8ed83fc58a4c
Karan8113 <157044092+Karan8113@users.noreply.github.com>
2025-01-30 01:44:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (93.59%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,38 +1,56 @@
 class Solution {
 public:
+    vector<int> parent;
+    vector<int> rank;
 
-    bool dfs(unordered_map<int,vector<int>> &mp,int u,int v,vector<bool>&visited){
-        visited[u]=true;
-        if(u==v)return true;
+    int find(int x){
+        if(x==parent[x])return x;
 
-        for(auto it:mp[u]){
-            if(!visited[it]){
-                if(dfs(mp,it,v,visited))return true;
-            }
+        return parent[x]=find(parent[x]);
+    }
+
+    void Union(int x,int y){
+        int x_parent=find(x);
+        int y_parent=find(y);
+
+        if(x_parent==y_parent)return;
+
+        if(rank[x_parent]>rank[y_parent]){
+            parent[y_parent]=x_parent;
         }
-
-        return false;
+        else if(rank[x_parent]<rank[y_parent]){
+            parent[x_parent]=y_parent;
+        }
+        else{
+            parent[x_parent]=y_parent;
+            rank[y_parent]++;
+        }
     }
 
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
+        parent.resize(n+1);
+        rank.resize(n+1);
 
-        unordered_map<int,vector<int>> mp;
+        for(int i=0;i<n+1;i++){
+            parent[i]=i;
+            rank[i]=1;
+        }
 
-        for(auto edge:edges){
+        for(auto &edge:edges){
             int u=edge[0];
             int v=edge[1];
 
-           vector<bool> visited(n,false);
+            int u_parent=find(u);
+            int v_parent=find(v);
 
-            if(mp.find(u)!=mp.end() && mp.find(v)!=mp.end() && dfs(mp,u,v,visited)){
-                return edge;
+            if(u_parent==v_parent){
+                return {u,v};
             }
 
-            mp[v].push_back(u);
-            mp[u].push_back(v);
+            Union(u,v);
         }
 
-        return {};
+        return {-1,-1};
     }
 };
