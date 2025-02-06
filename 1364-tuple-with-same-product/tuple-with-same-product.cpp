d11c00b5ceac59ad73f7d93973ccdab9c330eab8
Karan8113 <157044092+Karan8113@users.noreply.github.com>
2025-02-06 23:11:38 +0530

    Time: 307 ms (71.16%) | Memory: 85.2 MB (23.20%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -11,14 +11,8 @@ public:
         }
 
         for(auto it:mp){
-            if(it.second==2){
-                count+=8;
-            }
-            else if(it.second>2){
-
-                int x=((it.second-1)*(it.second))/2;
-                count+=(x*8);
-            }
+            int x=((it.second-1)*(it.second))/2;
+            count+=(x*8);
         }
         return count;
     }
