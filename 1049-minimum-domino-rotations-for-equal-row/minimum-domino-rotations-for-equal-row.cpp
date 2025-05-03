6fd576321e22efafffff91fc68da008f5d3b0ffa
Karan8113 <157044092+Karan8113@users.noreply.github.com>
2025-05-04 01:15:45 +0530

    Time: 4 ms (56.50%) | Memory: 115.5 MB (49.25%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
@@ -4,30 +4,40 @@ public:
         int n=tops.size();
         int ans=INT_MAX;
 
-        unordered_map<int,int> top;
-        unordered_map<int,int> bottom;
+        // unordered_map<int,int> top;
+        // unordered_map<int,int> bottom;
 
 
-        for(int i=0;i<n;i++){
-            top[tops[i]]++;
-            bottom[bottoms[i]]++;
-        }
+        // for(int i=0;i<n;i++){
+        //     top[tops[i]]++;
+        //     bottom[bottoms[i]]++;
+        // }
 
         for(int i=1;i<=6;i++){
             int isPossible=1;
+            int swapTop=0;
+            int swapBottom=0;
             int swap=n;
             for(int j=0;j<n;j++){
-                if(tops[j]==i || bottoms[j]==i){
+                if(tops[j]==i && bottoms[j]!=i){
+                    swapBottom++;
+                }
+                if(bottoms[j]==i && tops[j]!=i){
+                    swapTop++;
+                }
+                if(tops[j]==i && bottoms[j]==i){
                     continue;
                 }
-                else{
+                if(tops[j]!=i && bottoms[j]!=i){
                     isPossible=0;
                     break;
                 }
             }
             if(isPossible==1){
-                int occurence=max(top[i],bottom[i]);
-                swap=min(swap,n-occurence);
+                // int occurence=max(top[i],bottom[i]);
+                // cout<<swapTop<<"  "<<swapBottom<<endl;
+                int minSwaps=min(swapTop,swapBottom);
+                swap=min(swap,minSwaps);
                 ans=min(ans,swap);
             }
         }
