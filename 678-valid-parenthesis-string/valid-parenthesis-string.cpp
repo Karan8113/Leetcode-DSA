0b67cffa3391a8717aac45dbb898e08a2d6d76a3
Karan8113 <157044092+Karan8113@users.noreply.github.com>
2025-07-05 15:22:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (46.83%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
-int n;
-    vector<vector<int>> dp;
-    bool solve(string &s,int idx,int count){
-        if(idx==n){
-            if(count==0)return true;
-            return false;
-        }
-        if(count<0 )return false;
-
-        if(dp[idx][count]!=-1)return dp[idx][count];
-
-        bool ans;
-        if(s[idx]=='(')
-        ans = solve(s,idx+1,count+1);
-        
-
-        if(s[idx]==')')
-        ans = solve(s,idx+1,count-1);
-
-        if(s[idx]=='*'){
-            bool first = solve(s,idx+1,count+1);
-            
-            bool sec=solve(s,idx+1,count-1);
-            
-            bool third=solve(s,idx+1,count);
-           
-
-            ans= first || sec;
-            ans = ans || third;
-        }
-
-        return dp[idx][count]=ans;
-    }
-
     bool checkValidString(string s) {
-        n=s.length();
-        dp= vector<vector<int>> (n+1,vector<int>(n+1,-1));
-        return solve(s,0,0);
+        int n=s.length();
+        int min=0,maxx=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                min++,maxx++;
+            }
+            else if(s[i]==')'){
+                min=max(0,min-1);
+                maxx--;
+            }
+            else{
+                min=max(0,min-1);
+                maxx++;
+            }
+            if(maxx<0)return false;
+        }
+
+        return min==0;
     }
 };
