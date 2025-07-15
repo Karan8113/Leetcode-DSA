00e58c1fd1e5331550b922465df3a3949a9a4060
Karan8113 <157044092+Karan8113@users.noreply.github.com>
2025-07-15 11:20:05 +0530

    Time: 3 ms (11.41%) | Memory: 8.5 MB (11.89%) - LeetSync

---
+++ 3396-valid-word/valid-word.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+
+    bool isValid(string word) {
+        if(word.length()<3)return false;
+        bool vowel=false,consonent=false;
+        unordered_set<char> st;
+        st.insert('a');
+        st.insert('e');
+        st.insert('i');
+        st.insert('u');
+        st.insert('o');
+        st.insert('A');
+        st.insert('E');
+        st.insert('I');
+        st.insert('U');
+        st.insert('O');
+        for(int i=0;i<word.length();i++){
+            if(!isalnum(word[i]))return false;
+            if(st.count(word[i]))vowel=true;
+            if(st.count(word[i])==0 && !isdigit(word[i]))consonent=true;
+        }
+
+        if(vowel && consonent)return true;
+        return false;
+
+    }
+};
